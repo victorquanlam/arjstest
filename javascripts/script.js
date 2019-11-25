56a56769273ed8c212d46ae6191e7e0946cd2c9e
QuanLam <37169648+hiendaica11145@users.noreply.github.com>
2019-11-26 02:49:19 +1100

    asdf

--- javascripts/script.js
+++ javascripts/script.js
@@ -57,10 +57,11 @@ async function loadPlaceFromAPIs(position) {
 
 // add random marker
 function addRandomMarker() {
+    if(currentLocation){
     var randomLat = currentLocation.latitude + Math.floor(Math.random() * (0.000000001 - 0.0000000001)) + 0.0000000001
     var randomLng = currentLocation.longitude + Math.floor(Math.random() * (0.000000001 - 0.0000000001)) + 0.0000000001
     const locationsRef = database.ref('locations');
-    if(currentLocation){
+    
         console.log(currentLocation)
         locationsRef.push({
             name: 'Random Test Name',
